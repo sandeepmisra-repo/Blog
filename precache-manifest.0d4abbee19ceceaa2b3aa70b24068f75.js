c9e561ed16ce7bd7e262c2db182d154009176f2e
unknown <sandeep.misra96@gmail.com>
2020-11-26 13:50:59 +0530

    Updates

---
+++ precache-manifest.0d4abbee19ceceaa2b3aa70b24068f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac20dfccb7ecaafd6b0077e7183b6bf",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "57eecd907f36c47d1200",
+    "url": "/blog/static/js/2.6857e82f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/blog/static/js/2.6857e82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e7630188c25988e844",
+    "url": "/blog/static/js/main.273b5ead.chunk.js"
+  },
+  {
+    "revision": "df3e13a9bb3c80ee4f21",
+    "url": "/blog/static/js/runtime-main.c4d99124.js"
+  }
+]);
